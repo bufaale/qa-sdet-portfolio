5cfdb36dcd647efcc3081207d377111c4da0adb3
Alejandro Bufarini <alejandroebufarini@gmail.com>
2025-05-19 16:23:42 -0300

    Update dependencies in package.json and package-lock.json; format vite.config.js

--- modern/vite.config.js
+++ modern/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/qa-sdet-portfolio/' // para GitHub Pages
-})
+  base: '/qa-sdet-portfolio/', // 👈 esto es clave para GitHub Pages
+  plugins: [react()]
+});
